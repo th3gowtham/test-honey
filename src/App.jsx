d368e57d9c47a3c5075ba6225876b23002c6e17f
lidhuna.g@prasklatechnology.com <lidhunag03@gmail.com>
2025-07-04 10:33:46 +0530

    Initial commit: React + Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    
+       <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold text-blue-600">React + Vite + Tailwind CSS</h1>
+    </div>
+    
+  )
+}
+
+export default App
